adf006924e0cccc70ccdb7008e3a74a6a176567d
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-06-12 13:54:36 +0400

    authentification done

---
+++ lib/screens/chat.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+//Deme@gmail.com
+//bla123
+
+//test@gmail.com
+//bla123123
+
+//test1@gmail.com
+//blablabla123
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: const Center(child: Text('Logged in!')),
+    );
+  }
+}
